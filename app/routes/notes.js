6b847df82907871368b225898cc36d3ba1d2c9fb
Dailton-Mac <dailtonbastos@gmail.com>
2019-12-22 22:39:49 -0300

    Baixando a lista de notas

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -30,6 +30,15 @@ router.get("/:id", withAuth, async (req, res) => {
   }
 });
 
+router.get("/", withAuth, async (req, res) => {
+  try {
+    let notes = await Note.find({ author: req.user._id });
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
 const isOwner = (user, note) => {
   if (JSON.stringify(user._id) == JSON.stringify(note.author._id)) {
     return true;
